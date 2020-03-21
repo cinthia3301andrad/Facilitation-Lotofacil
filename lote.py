493fc9d7bf770bedc3caa7bb6914d7cddc763119
Sindyme <cinthiaadm15@gmail.com>
2020-03-21 16:49:01 -0300

    Add lote.py

---
+++ lote.py
@@ -0,0 +1,69 @@
+import random
+def adicionar():
+    num = input(f'Digite a sequencia: [separados por espaço]')
+    n = num.split()
+    return n
+
+def saber(listanum):
+    listsorteados = list()
+    sortnum = input('Digite a sequência a ser comparada: [separados por espaço] ')
+    sortnumL = sortnum.split()
+    if len(listanum) == len(sortnumL):
+        for i in range(0, len(listanum)):
+            if sortnumL[i] in listanum:
+                listsorteados.append(sortnumL[i])
+    else:
+        print('Desculpe, seu jogo está incorreto!')
+        return 'JOGO ERRADO!'
+    return listsorteados
+
+def imprimir_jogo(tabnum):
+    matriz = [] #lista vazia
+    n = 0
+    for i in range(5):
+        #cria a linha i
+        linha = []
+        for j in range(3):
+            linha.append(tabnum[n])
+            n += 1
+        matriz.append(linha)
+    return matriz
+
+def gerador_aleatorio():
+    random.seed()
+    listAlea = []
+    cont = 0
+    while cont != 15:
+        numAlea = random.randrange(1, 25)
+        if numAlea not in listAlea:
+            listAlea.append(numAlea)
+            cont += 1
+    return listAlea
+
+
+print('------------------- JOGO OFICIAL -------------------')
+print('A seguir, digite o jogo oficial que será comparado\ncom os jogos que você fez')
+print('----------------------------------------------------')
+# testA = gerador_aleatorio()
+
+
+# print( 'Teste aleatorio : ', testA)
+numeros = adicionar()
+while True:
+    teste = saber(numeros)
+    tabela = imprimir_jogo(numeros)
+    #printar tabela
+    print(f'Sequência original: ')
+    for i in range(5):
+        for j in range(3):
+            print(f'   {tabela[i][j]}   ', end='')
+        print('\n')
+    print('=-='*15)
+
+    print(f'ACERTOS     : {len(teste)}')
+    print(f'Sequencia de acertos     : {teste}')
+    print('=-='*15)
+    cond = str(input('Deseja verificar outro jogo? s / n : '))
+    if cond == 'n':
+        break
+    
